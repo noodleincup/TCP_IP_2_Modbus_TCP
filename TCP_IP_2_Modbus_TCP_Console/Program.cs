4dc57efdf5d21bc642bad31ece709cc68132ba32
NoodleInCup <kkukkong102544@gmail.com>
2025-10-02 21:13:28 +0700

    Modified read setting file function and make code clean

--- TCP_IP_2_Modbus_TCP_Console/Program.cs
+++ TCP_IP_2_Modbus_TCP_Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EasyModbus;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,10 +10,13 @@ namespace TCP_IP_2_Modbus_TCP_Console
     internal class Program
     {
         private static ModbusServer ModbusServer;
-        const int PORT = 8080;
-        const int START_READ_SETTING_SERVER = 1;
         
+        const int START_READ_SETTING_SERVER = 2;
+        const int ADDRESS_PER_CONNECTION = 10;
+
+        static string settingFilePath = AppDomain.CurrentDomain.BaseDirectory.Trim('\\') + @"\NetworkServer.txt";
         static byte connection_num = 0;
+        static int modbusPort = 8080;
 
         //private static bool shouldStop { get; set; } = false;
         private static volatile bool _shouldStop = false;
@@ -24,34 +28,25 @@ namespace TCP_IP_2_Modbus_TCP_Console
             // Create a Modbus server instance
             InitializeModbusServer();
 
-            // Read connections from user
-            //var networkObjectArray = ReadConnectionsFromUser();
-            //var networkObjectArray = new NetworkObject[2];
-            //networkObjectArray[0] = new NetworkObject { IPAddress = "192.168.1.9", Port = 9000 };
-            //networkObjectArray[1] = new NetworkObject { IPAddress = "192.168.1.23", Port = 9000 };
-
-            string appPath = AppDomain.CurrentDomain.BaseDirectory.Trim('\\');
-            string settingPath = appPath + @"\NetworkServer.txt";
-            var networkObjectArray = ReadNetworkObjectArray(settingPath);
+            // Read Network settings from file
+            if (!File.Exists(settingFilePath))
+            {
+                Console.WriteLine($"Setting file not found at '{settingFilePath}'");
+            }
+            var networkObjectArray = ReadNetworkObjectArray(settingFilePath);
 
             if (networkObjectArray.Length == 0 )
             {
                 Console.WriteLine("No valid server connections found. Exiting.");
             }
-            else if (!File.Exists(settingPath))
-            {
-                Console.WriteLine("Setting file not found. Exiting.");
-            }
             else
             {
                 // Get connection number from users
                 Console.WriteLine($"Number of connection: {connection_num}");
-                //int connection_num = (int.TryParse(Console.ReadLine(), out int num) ? num : 0);
-
 
                 // Wait for user to press Enter
-                Console.WriteLine("Press Enter to Start");
-                Console.ReadLine();
+                //Console.WriteLine("Press Enter to Start");
+                //Console.ReadLine();
 
                 // Create TCP clients and connect to servers
                 TcpClient[] clients = new TcpClient[connection_num];
@@ -59,7 +54,7 @@ namespace TCP_IP_2_Modbus_TCP_Console
                 // Register shutdown handlers
                 RegisterShutdownHandlers(clients);
 
-                //Thread[] threads = StartWorkerThreads(networkObjectArray, client, connection_num);
+                // Create threads array
                 Thread[] threads = new Thread[connection_num];
 
                 // Start threads for each connection
@@ -67,7 +62,6 @@ namespace TCP_IP_2_Modbus_TCP_Console
                 {
                     int index = i; // Capture for lambda
                     clients[index] = new TcpClient();
-                    //threads[index] = new Thread(() => ThreadClient(ModbusServer, clients[index], index));
                     threads[index] = new Thread(() => ThreadMethod(ModbusServer, clients[index], networkObjectArray[index], index + 1));
                     threads[index].Start();
                     Console.WriteLine($"Thread {index} starting");
@@ -92,12 +86,13 @@ namespace TCP_IP_2_Modbus_TCP_Console
 
         private static void InitializeModbusServer()
         {
+            modbusPort = ReadModbusPort(settingFilePath);
             ModbusServer = new ModbusServer
             {
-                Port = PORT
+                Port = modbusPort
             };
             ModbusServer.Listen();
-            Console.WriteLine($"Modbus TCP Server is listening on port {PORT}");
+            Console.WriteLine($"Modbus TCP Server is listening on modbusPort {modbusPort}");
         }
 
         private static void RegisterShutdownHandlers(TcpClient[] clients)
@@ -140,6 +135,7 @@ namespace TCP_IP_2_Modbus_TCP_Console
             };
         }
 
+        #region Backup Function
         private static NetworkObject[] ReadConnectionsFromUser()
         {
             // Get number of connections TCP/IP Servers
@@ -199,7 +195,7 @@ namespace TCP_IP_2_Modbus_TCP_Console
 
             return threads;
         }
-
+        #endregion
         static void ThreadMethod(ModbusServer modbusServer, TcpClient client, NetworkObject networkObject, int connection_num)
         {
             string serverIP = networkObject.IPAddress;
@@ -235,36 +231,24 @@ namespace TCP_IP_2_Modbus_TCP_Console
                         Console.WriteLine($"Buffer as hex: {bytesAsString}");
                         Console.WriteLine($"Received {bytesRead} bytes from server.");
 
+                        // Check for disconnection
                         if (bytesRead == 0)
                         {
                             Console.WriteLine("No data received, closing connection.");
                             break;
                         }
 
-                        // Trim STX (0x02) and ETX (0x03)
-                        int startIndex = 0;
-                        int endIndex = bytesRead;
+                        // Trim STX (0x02) and ETX (0x03) and get clean data bytes
+                        byte[] cleanData = getCleanDataBytes(buffer, bytesRead);
 
-                        // Check first byte
-                        if (bytesRead > 0 && buffer[0] == 0x02)
-                            startIndex++;
-
-                        // Check last byte
-                        if (bytesRead > 1 && buffer[bytesRead - 1] == 0x03)
-                            endIndex--;
-
-                        // Create clean data array
-                        byte[] cleanData = new byte[endIndex - startIndex];
-                        Array.Copy(buffer, startIndex, cleanData, 0, cleanData.Length);
-
-
+                        // Convert clean data to string
                         string response = Encoding.UTF8.GetString(cleanData);
-
-
                         Console.WriteLine($"{serverIP} response: " + response);
 
                         // Register weight data to Modbus Server
                         string weightData = getWeightData(response);
+
+                        // Register weight data to Modbus Server
                         if (weightData != "")
                         {
                             RegisterWeightToModbusServer(modbusServer, weightData, connection_num);
@@ -390,6 +374,26 @@ namespace TCP_IP_2_Modbus_TCP_Console
             return (res.Substring(0, 3) == "*02") ? res.Substring(4).Replace("G", "") : "";
         }
 
+        static byte[] getCleanDataBytes(byte[] data, int bytesRead)
+        {
+            // Trim STX (0x02) and ETX (0x03)
+            int startIndex = 0;
+            int endIndex = bytesRead;
+
+            // Check first byte
+            if (bytesRead > 0 && data[0] == 0x02)
+                startIndex++;
+
+            // Check last byte
+            if (bytesRead > 1 && data[bytesRead - 1] == 0x03)
+                endIndex--;
+
+            // Create clean data array
+            byte[] cleanData = new byte[endIndex - startIndex];
+            Array.Copy(data, startIndex, cleanData, 0, cleanData.Length);
+            return cleanData;
+        }
+
         static void RegisterWeightToModbusServer(ModbusServer modbusServer, string weightData, int connection_num)
         {
             // Convert weight data to integer (assuming it's a valid number)
@@ -399,10 +403,10 @@ namespace TCP_IP_2_Modbus_TCP_Console
             {
                 // Register the weight data to Modbus server at the specified address
                 ModbusServer.HoldingRegisters reg = modbusServer.holdingRegisters; // Uncomment and use actual Modbus server instance
-                reg[3 * offsetAddress + 1] = weightInt; // Store weight at address corresponding to connection number
-                reg[3 * offsetAddress + 2] = weightFloat;
-                Console.WriteLine($"Registered weight {weightInt} at Modbus address {3 * offsetAddress + 1}");
-                Console.WriteLine($"Registered weight {weightFloat} at Modbus address {3 * offsetAddress + 2}");
+                reg[ADDRESS_PER_CONNECTION * offsetAddress + 1] = weightInt; // Store weight at address corresponding to connection number
+                reg[ADDRESS_PER_CONNECTION * offsetAddress + 2] = weightFloat;
+                Console.WriteLine($"Registered weight {weightInt} at Modbus address {ADDRESS_PER_CONNECTION * offsetAddress + 1}");
+                Console.WriteLine($"Registered weight {weightFloat} at Modbus address {ADDRESS_PER_CONNECTION * offsetAddress + 2}");
             }
             else
             {
@@ -414,7 +418,7 @@ namespace TCP_IP_2_Modbus_TCP_Console
         {
             int offsetAddress = connection_num - 1;
             ModbusServer.HoldingRegisters reg = modbusServer.holdingRegisters;
-            reg[3 * offsetAddress + 3] = (short)status;
+            reg[ADDRESS_PER_CONNECTION * offsetAddress + 3] = (short)status;
         }
 
         static NetworkObject[] ReadNetworkObjectArray(string filePath)
@@ -423,7 +427,7 @@ namespace TCP_IP_2_Modbus_TCP_Console
             {
                 string[] lines = File.ReadAllLines(filePath);
 
-                connection_num = (byte.TryParse(lines[0], out byte num)) ? num : (byte)0;
+                connection_num = (byte.TryParse(lines[1], out byte num)) ? num : (byte)0;
 
                 NetworkObject[] networkObjects = new NetworkObject[lines.Length];
                 for (int i = START_READ_SETTING_SERVER; i < lines.Length; i++)
@@ -432,7 +436,7 @@ namespace TCP_IP_2_Modbus_TCP_Console
                     if (parts.Length == 3 && int.TryParse(parts[2], out int port))
                     {
                         networkObjects[i] = new NetworkObject { IPAddress = parts[1], Port = port };
-                        Console.WriteLine($"Read connection {i + 1}: IP={parts[1]}, Port={port}");
+                        Console.WriteLine($"Read connection : IP={parts[1]}, Port={port}");
                     }
                     else
                     {
@@ -454,6 +458,38 @@ namespace TCP_IP_2_Modbus_TCP_Console
             }
             return Array.Empty<NetworkObject>();
 
+        }
+
+        static int ReadModbusPort(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                if (lines.Length > 0)
+                {
+                    string modbusLines = lines[0];
+                    if (!int.TryParse(modbusLines.Split(" ")[1], out int port) || port < 0 || port > 65535)
+                    {
+                        Console.WriteLine($"Invalid Modbus port in file. Using default port {modbusPort}.");
+                        return modbusPort;
+                    }
+                    return port;
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid Modbus port in file. Using default port {modbusPort}.");
+                    return modbusPort;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Error: File not found at '{filePath}'");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error reading file: {ex.Message}");
+            }
+            return modbusPort;
         }
 
 
